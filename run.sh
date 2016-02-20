6d0b1889f0990db9259d57c9deddfea6c98628e3
rosszhang <yihao.ross@gmail.com>
2016-02-20 11:58:55 -0500

    - Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+rm -rf docker-stacks
+
 git clone https://github.com/jupyter/docker-stacks.git
 
 cd docker-stacks
 
-docker run -d -p 8888:8888 -p 4040:4040 -p 4041:4041 -e GRANT_SUDO=yes -v /home/vagrant/workspace/docker/spark/docker-pyspark-cluster/work:/home/jovyan/work jupyter/all-spark-notebook
+docker run -d -p 8888:8888 -p 8088:8088 -p 8042:8042 -p 8080:8080 -p 8081:8081 -p 7077:7077 -p 4040:4040 -p 4041:4041 -p 18080:18080 -e GRANT_SUDO=yes -v /home/vagrant/workspace/docker/spark/docker-pyspark-cluster/work:/home/jovyan/work jupyter/all-spark-notebook
